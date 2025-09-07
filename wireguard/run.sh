32190c0f44bfd34055cdbe34d17d83cd106073cc
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2025-09-07 09:18:25 +0900

    Squashed commit of the following:
    commit 8496c88e428254e9175655cd7471378f66d692ff
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Sep 7 09:07:13 2025 +0900
    
        fix volume dir
    
    commit 7750d9c1698d72cb1c6b170803d193f0ca06744e
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Sep 7 08:50:19 2025 +0900
    
        fix logger and rename volume name
    
    commit c96154df879ef01a74ad6a67d7b765ee27ce4c6e
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Sep 7 08:34:37 2025 +0900
    
        fix pod name
    
    commit 7e8293f97bbb4a1cdc4a580b34737b2d60fae7ba
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Sep 7 08:00:26 2025 +0900
    
        fix Readme
    
    commit f779e7cfc0d0cbb019c95b374eb09881881cc358
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Sep 7 07:56:37 2025 +0900
    
        Revert "add network"
    
        This reverts commit 5835e7915eb2f8228a85a5708b79ba573481132a.
    
    commit 5835e7915eb2f8228a85a5708b79ba573481132a
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Wed Aug 27 21:50:09 2025 +0900
    
        add network
    
    commit ba98014cdb5e6f2c51aa716c340627f6ddef15a5
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Aug 24 22:18:43 2025 +0900
    
        fix systemd unit file
    
    commit 731737c5750cfeb0bac5d8ad188945393e6056d8
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Aug 24 22:03:17 2025 +0900
    
        fix systemd file
    
    commit 62b22d91ac01f421c4cf29d5f78cf430ece51042
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Aug 24 18:16:53 2025 +0900
    
        add logger enable command in Readme
    
    commit 735c067736b09e41a23f3b01a92bd3169e67219b
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Aug 24 18:08:38 2025 +0900
    
        add systemd unit file for logger
    
    commit 7fd8c8ebf12d6566d853a3e4bc6645c4234ac5e2
    Merge: 97933f7 c05e569
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Sun Aug 24 18:07:39 2025 +0900
    
        Merge branch 'main' into dev
    
    commit 97933f76410b7adecceaae2d142db803c00ff357
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Fri Aug 22 04:37:10 2025 +0900
    
        fix readme
    
    commit 1369cd046c669de47cb8edffdf7d4c461cc03120
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Fri Aug 22 04:26:29 2025 +0900
    
        add ServiceName in quadlet
    
    commit 30f90b067f850054190fa37248b155846051ca72
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Fri Aug 22 04:04:28 2025 +0900
    
        fix Readme
    
    commit b79cf900eff4d4a772f08559b73b5d42b8b20efa
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Fri Aug 22 03:34:51 2025 +0900
    
        fix copy command in dockerfile
    
    commit 1a45673f9dfeb5c0e5ef4f6a861976a9a020489f
    Author: HNTKAR <45356209+HNTKAR@users.noreply.github.com>
    Date:   Fri Aug 22 03:31:16 2025 +0900
    
        add build quadlet

--- wireguard/run.sh
+++ wireguard/run.sh
@@ -9,20 +9,20 @@ container_exit() {
 }
 
 trap "container_exit" SIGTERM
-mkdir -p /usr/$POD_NAME/$CONTAINER_NAME/{key,conf,logs}
-chown $(id -u):$(id -u) -R /usr/$POD_NAME/$CONTAINER_NAME
-chmod 700 /usr/$POD_NAME/$CONTAINER_NAME
+mkdir -p /usr/V/$CONTAINER_NAME/{key,conf,logs}
+chown $(id -u):$(id -u) -R /usr/V/$CONTAINER_NAME
+chmod 700 /usr/V/$CONTAINER_NAME
 
 NIC_NAME=$(ip addr|grep BROADCAST|cut -d ":" -f 2|sed s/\ //g|sed s/\@.*//g|head -n 1)
 ip link add dev wg0 type wireguard
 ip address add dev wg0 $VPN_NET
 
-if [ ! -f /usr/$POD_NAME/$CONTAINER_NAME/conf/wg0.conf ]; then
-    wg genkey | tee /usr/$POD_NAME/$CONTAINER_NAME/key/private.key | wg pubkey > /usr/$POD_NAME/$CONTAINER_NAME/key/public.key
-    wg set wg0 listen-port $PORT private-key /usr/$POD_NAME/$CONTAINER_NAME/key/private.key
-    wg showconf wg0 > /usr/$POD_NAME/$CONTAINER_NAME/conf/wg0.conf
+if [ ! -f /usr/V/$CONTAINER_NAME/conf/wg0.conf ]; then
+    wg genkey | tee /usr/V/$CONTAINER_NAME/key/private.key | wg pubkey > /usr/V/$CONTAINER_NAME/key/public.key
+    wg set wg0 listen-port $PORT private-key /usr/V/$CONTAINER_NAME/key/private.key
+    wg showconf wg0 > /usr/V/$CONTAINER_NAME/conf/wg0.conf
 else
-    wg syncconf wg0 /usr/$POD_NAME/$CONTAINER_NAME/conf/wg0.conf
+    wg syncconf wg0 /usr/V/$CONTAINER_NAME/conf/wg0.conf
 fi
 
 ip link set down dev wg0
@@ -30,7 +30,7 @@ ip link set up dev wg0
 
 echo "WireGuard interface wg0 created with IP $VPN_NET"
 echo "Listening on port $PORT"
-echo "wireguard public key: $(cat /usr/$POD_NAME/$CONTAINER_NAME/key/public.key)"
+echo "wireguard public key: $(cat /usr/V/$CONTAINER_NAME/key/public.key)"
 
 iptables -t nat -A POSTROUTING -o $NIC_NAME -j MASQUERADE
 iptables -A FORWARD -i wg0 -j ACCEPT
